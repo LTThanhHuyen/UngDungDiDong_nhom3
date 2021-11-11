51a0c56007adfa0272bd268311249d6e9ad93e13
LTThanhHuyen <lethithanhhuyen2000@gmail.com>
2021-11-12 00:48:16 +0700

    first commit

---
+++ btkiemtra/lib/models/topic.dart
@@ -0,0 +1,14 @@
+class Topic {
+  String name;
+  String backgroundUrl;
+
+  Topic(this.name, this.backgroundUrl);
+}
+//import 'package:flutter_news/models/topic.dart';
+List<Topic> topics = [
+  Topic('Business', 'https://img.lovepik.com/photo/50047/8472.jpg_wh860.jpg'),
+  Topic('Entertainment',
+      'https://e4life.vn/wp-content/uploads/2021/05/tu-vung-ielts-chu-de-entertainment-4Life-English-Center.jpg'),
+  Topic('General', 'https://danangfantasticity.com/wp-content/uploads/2015/10/bien-danang.jpg'),
+  Topic('Sports', 'https://img.lovepik.com/photo/40100/4266.jpg_wh860.jpg'),
+];
